9c2c551f17740fb257866548605ca9e93841a616
AbdulRahman Armashi <abod3e4@gmail.com>
2020-11-11 10:45:57 +0200

    Finished Unit Class

--- src/com/destroyordefend/project/Main.java
+++ src/com/destroyordefend/project/Main.java
@@ -3,6 +3,11 @@ package com.destroyordefend.project;
 public class Main {
 
     public static void main(String[] args) {
+
+
         System.out.println("First Commit");
+        System.out.println("Armashi");
     }
+
+
 }
