ffd37824ad624d0f951ecaef78652595ead0b9e8
dolorsy <47667168+dolorsy@users.noreply.github.com>
2020-11-13 13:49:58 +0200

    First Real Commit in The Project

--- src/com/destroyordefend/project/Main.java
+++ src/com/destroyordefend/project/Main.java
@@ -3,14 +3,10 @@ package com.destroyordefend.project;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First Commit");
-        System.out.println("Armashi");
-        //hello
 
-        System.out.println("Hariri Here!!");
-        //hariri
-        System.out.println("Are Yoy Ready?");
     }
 
 
 }
+
+
