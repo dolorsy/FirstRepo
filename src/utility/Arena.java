7bb565a9e2fc27b9d086e382f0c58a17a185185d
dolorsy <47667168+dolorsy@users.noreply.github.com>
2020-11-13 14:49:26 +0200

    Some Essential Add

--- src/utility/Arena.java
+++ src/utility/Arena.java
@@ -1,4 +1,16 @@
 package utility;
 
 public class Arena {
+    static Byte[][] arena = new Byte[10000][10000];
+
+
+
+    boolean setUnitPlace(Point center,int range){
+    //Todo: Here should proccess of placing Units on arena; return true if can ,or false to rechoose
+
+
+        return false;
+    };
+
+
 }
