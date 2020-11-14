cf7c2a97375aa4acdb3e431e24f87c19a1783aea
AbdulRahman Armashi <abod3e4@gmail.com>
2020-11-14 20:59:46 +0200

    edit sort map parameter

--- src/com/destroyordefend/project/Tactic/HighestDamageAttack.java
+++ src/com/destroyordefend/project/Tactic/HighestDamageAttack.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
 public class HighestDamageAttack implements Tactic{
 
     @Override
-    public void SortMap(Map<Unit, Point> OnArena) {
+    public void SortMap(Map<Unit, Point> OnArena ) {
        // Map <Unit.UnitValues.getDamage(),Point> temp = new TreeMap<Unit,Point>(OnArena)
 
     }
