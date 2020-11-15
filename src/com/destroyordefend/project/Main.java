c7455950036a3a97ba52ef047ef63ab1ade5210b
0Ahmad <ah774129@gmail.com>
2020-11-15 12:25:13 +0200

    Armashi11

--- src/com/destroyordefend/project/Main.java
+++ src/com/destroyordefend/project/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("First Commit");
-        System.out.println("Armashi");
+        System.out.println("Armashi11");
         //hello
 
         System.out.println("Hariri Here!!");
