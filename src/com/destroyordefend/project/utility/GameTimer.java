4208047460be8e375443a8bed2b27a8f13a0028f
dolorsy <47667168+dolorsy@users.noreply.github.com>
2020-11-18 11:51:52 +0200

    Some Comments

--- src/com/destroyordefend/project/utility/GameTimer.java
+++ src/com/destroyordefend/project/utility/GameTimer.java
@@ -1,13 +1,22 @@
 package com.destroyordefend.project.utility;
 
+import static com.destroyordefend.project.utility.MainMethodAsyncTask.invokeMainMethods;
+import static com.destroyordefend.project.utility.UpdateMapAsyncTask.invokeUpdatePosition;
+import static com.destroyordefend.project.utility.UpdateRangeAsyncTask.invokeUpdateRange;
+
 public class GameTimer extends Thread {
 int RoundLength = 30;
 int currentSecond = 0;
     public void run(){
         for(int i=1;i<=RoundLength;i++){
             try {
+                //Todo: Be Careful About Time Of the Following Three Methods, it should be 0.9 Second For Them all Together
+                //Todo:May Be You need Exception Handling
+                invokeUpdatePosition();
+                invokeUpdateRange();
+                invokeMainMethods();
                 Thread.sleep(1000);
-                System.out.println(i);
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 //Todo: Need To Implement
