3f0b98ce498bbb80f25d8a54ee5d9c6fa10b5d48
0Ahmad <ah774129@gmail.com>
2020-12-05 10:56:04 +0200

    read

--- src/com/destroyordefend/project/Main.java
+++ src/com/destroyordefend/project/Main.java
@@ -1,6 +1,14 @@
 package com.destroyordefend.project;
 
 import com.destroyordefend.project.Core.Game;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
@@ -14,7 +22,5 @@ public class Main {
 //        gameTimer.start();
        Game game = Game.getGame();
         game.StartAnewGame();
-
-
     }
 }
